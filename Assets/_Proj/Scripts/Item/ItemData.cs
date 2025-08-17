0c55583bfb180559cccea0258172559abb82d7d0
H <ksturm960@gmail.com>
2025-08-17 13:03:00 +0900

    Missile Prefab <- NotShared, MissileProj <- need AIController(Branch processing required)
    MissileProj은 공통으로 사용할 수 있음.
    기타 ItemData도 공유해야 같은 효과 기대할 수 있음.

--- Assets/_Proj/Scripts/Item/ItemData.cs
+++ Assets/_Proj/Scripts/Item/ItemData.cs
@@ -8,7 +8,8 @@ public class ItemData : ScriptableObject
   public ItemType type;
 
   public Sprite icon;
-  public GameObject fxPrefab;
+  public GameObject fxPrefab; // 아이템 사용 시 나오는 효과 (아이템 효과 X)
+  // 미사일의 경우, 폭발 시 나오는 효과
 
   public float duration; // Maintain duration
   public float power; // Booster <- Add Speed, Missile <- Add Damage
