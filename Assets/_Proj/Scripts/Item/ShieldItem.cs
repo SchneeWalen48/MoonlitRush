faba11547685212331736d66f8786810f9f72d2a
H_KIcom <>
2025-08-18 10:50:21 +0900

    shield, missile, booster.cs, item datas
    # Conflicts:
    #	Assets/_Proj/Scenes/PlayerTest.unity
    #	Assets/_Proj/Scenes/SampleScene/Global Volume Profile.asset
    #	Assets/_Proj/Scripts/Item/Booster.asset
    #	Assets/_Proj/Scripts/Item/Missile.asset
    #	Assets/_Proj/Scripts/Item/UseItem.cs

---
+++ Assets/_Proj/Scripts/Item/ShieldItem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ShieldItem : MonoBehaviour
+{
+  private bool isShield;
+  private GameObject fxPrefab;
+
+  public void Activate(ItemData data)
+  {
+    if (isShield) return;
+
+    StartCoroutine(ShieldCoroutine(data.duration, data.fxPrefab ));
+  }
+
+  IEnumerator ShieldCoroutine(float duration, GameObject fx)
+  {
+    if(fx != null)
+    {
+      fxPrefab = Instantiate(fx, transform.position, Quaternion.identity, transform);
+    }
+
+    //TODO : 무적 처리
+
+    yield return new WaitForSeconds(duration);
+
+    if (fx != null)
+    {
+      Destroy(fxPrefab);
+    }
+
+    isShield = false;
+  }
+}
---
+++ Assets/_Proj/Scripts/UI/SlotUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SlotUI : MonoBehaviour
+{
+  public Image icon;
+
+  public void SetItem(ItemData data)
+  {
+    if(data != null || data.icon != null)
+    {
+      icon.sprite = data.icon;
+      icon.enabled = true;
+    }
+    else
+    {
+      icon.enabled = false;
+    }
+  }
+}
